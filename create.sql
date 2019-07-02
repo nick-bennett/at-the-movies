f909a110c5e3de6ec5e9da89755728df4f8e2ab1
Nick Bennett <nick@nickbenn.com>
2019-07-02 10:29:30 -0600

    Start Actor portion of MVC.

---
+++ create.sql
@@ -0,0 +1,1 @@
+create table movie (movie_id CHAR(16) FOR BIT DATA not null, created timestamp not null, genre varchar(255), screenwriter varchar(255), title varchar(255) not null, updated timestamp not null, primary key (movie_id))
